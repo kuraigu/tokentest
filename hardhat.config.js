138002ecce1eaeb7a1129f1aab5a33103df25a54
Innoh Reloza <southsoftstudio@gmail.com>
2022-08-23 04:26:02 +0800

    Modified Hardhat config to support different compiler versions
    Modified Hardhat config to support different compiler versions

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,16 @@ require("@nomiclabs/hardhat-ethers");
 const { API_URL, PRIVATE_KEY1 } = process.env;
 
 module.exports = {
-   solidity: "0.7.3",
+   solidity: {
+      compilers: [
+         {
+            version: "0.8.0"
+         },
+         {
+            version: "0.8.1"
+         }
+      ],
+   },
    defaultNetwork: "goerli",
    networks: {
       hardhat: {},
@@ -16,5 +25,9 @@ module.exports = {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY1}`]
       }
+   },
+
+   paths: {
+      artifacts: "./artifcacts"
    },
 }
